cd92d06b2f0b3a10a255f9dbe695090be9038292
Nolo Marsh <nolo.marsh@gmail.com>
2021-10-22 13:55:17 -0400

    users find-by-id route made

--- controllers/users.js
+++ controllers/users.js
@@ -3,8 +3,14 @@ const router = express.Router()
 postgres = require('../postgres.js')
 
 router.get('/', (req,res) => {
-    postgres.query('SELECT * FROM users ORDER BY id ASC;', (err, results) => {
+    postgres.query('SELECT username FROM users ORDER BY id ASC;', (err, results) => {
         res.json(results.rows)
+    })
+})
+
+router.get('/:id', (req,res) => {
+    postgres.query(`SELECT * FROM users WHERE id = ${req.params.id}`, (err, result) => {
+        res.json(result)
     })
 })
 
